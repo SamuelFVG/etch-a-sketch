cd6da3a4aef0dc5366b727af4568f4ac7cdde508
SamuelFVG <samuelfelipeif@gmail.com>
2022-01-12 08:56:35 -0300

    Fixed the dragging bug by preventing dragging with preventDefault() and improved functionality by adding an event listener for when the user clicks, but isn't moving the cursor

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 const grid = [];
-
+let shouldPaint = false;
 const mainContainer = document.querySelector("#main");
 
-createGrid(4, 4);
+createGrid(40, 40);
 /*
     This function creates a grid with an specific number of rows and columns, that is represented by an array of arrays. 
     Each column item from the grid is stored inside of the array of arrays in its specific position (row and column).
@@ -20,9 +20,28 @@ function createGrid(rows, columns){
             const column_item = document.createElement('div');
 
             grid[i].push(column_item); // push each column item inside of the specific row of index i in the array of arrays
-            column_item.textContent = `${i}x${j}`
+            //column_item.textContent = `${i}x${j}`
             column_item.setAttribute('class', `column-item _${i}_${j}`);
             row.appendChild(column_item);
         }
     }
 }
+
+function changeColor(event) {
+    if (shouldPaint) event.target.style.backgroundColor = 'black';
+}
+
+grid.forEach(row => { 
+    row.forEach(item => { 
+        item.addEventListener("mouseover", changeColor);
+    });
+});
+
+mainContainer.addEventListener('mousedown', e => {
+    shouldPaint = true;
+    changeColor(e);
+});
+document.addEventListener('mouseup', () => shouldPaint = false);
+
+const alldivs = document.querySelectorAll('div');
+alldivs.forEach(div => div.addEventListener('dragstart', e => e.preventDefault()))
